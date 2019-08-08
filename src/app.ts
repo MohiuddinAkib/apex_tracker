40b3401d179bf01a7d1f91f5c2bfa1be7c79da84
md akib <mohiuddinmostafakamal@gmail.com>
2019-08-09 01:11:56 +0600

    managing config module, debuf module via environment vars

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("Hello world");
+// relect metadata
+import 'reflect-metadata'
+// dotenv
+import dotenv from 'dotenv'
+// config dotenv
+dotenv.config()
+
+// modules
+import debug from 'debug'
+import express from 'express'
+import config from 'config'
+
+// Init app
+const app = express()
+
+// Init app debug
+const appDebug = debug('app:main')
+
+// Set port
+app.set('port', config.get('APP_PORT'))
+
+// Listen for request
+const server = app.listen(app.get('port'));
+// app error handling
+server.on("listening", () => appDebug(`Server is running on port ${app.get('port')}`)).once('error', (error: Error) => {
+    appDebug('server error', error.name, error.message)
+    appDebug(error.stack)
+    process.exit(1)
+})
---
+++ src/config/production.ts
@@ -0,0 +1,4 @@
+export default {
+    TRACKER_NETWORK_API_KEY: process.env.TRACKER_NETWORK_API_KEY,
+    APP_PORT: process.env.APP_PORT
+}
