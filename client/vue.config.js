d48a90488a640a7f905c7bc73e18d6e00bd7f979
md akib <mohiuddinmostafakamal@gmail.com>
2019-08-11 02:02:09 +0600

    server files on root

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../server/dist/public"),
+  outputDir: path.resolve(__dirname, "../dist/public"),
   devServer: {
     proxy: {
       "/api/v1": {
