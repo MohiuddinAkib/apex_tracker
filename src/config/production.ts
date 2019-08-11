b85282386fc9f2b370db9f60b65b54617c99d7e5
md akib <mohiuddinmostafakamal@gmail.com>
2019-08-11 11:25:13 +0600

    removed dotenv import from production.ts config file

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,16 +1,3 @@
-// dotenv
-import dotenv from 'dotenv';
-// Path module
-import path from 'path';
-
-// constructing .env path
-const envPath = path.join(path.dirname(__dirname), '.env');
-
-// config dotenv
-dotenv.config({
-  path: envPath
-});
-
 module.exports = {
   TRACKER_NETWORK_API_URL: process.env.TRACKER_NETWORK_API_URL,
   TRACKER_NETWORK_API_KEY: process.env.TRACKER_NETWORK_API_KEY,
